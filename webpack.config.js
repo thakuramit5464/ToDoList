e2976168ac2e42301b1180e8a769270e0efdbd5b
thakuramit5464 <thakuramit5464@gmail.com>
2023-01-19 21:42:14 +0530

    Added Webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+var nodeEnv = process.env.NODE_ENV || "development";
+var isDev = nodeEnv !== "production";
+const CopyPlugin = require("copy-webpack-plugin");
+
+var config = {
+  mode: "development",
+  watch: true,
+  entry: {
+    dist: "./todoList.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "todoList.js",
+  },
+  resolve: {
+    extensions: [
+      ".tsx",
+      ".ts",
+      ".js",
+      ".json",
+      ".css",
+      ".sh",
+      ".babelrc",
+      ".eslintignore",
+      ".gitignore",
+      ".d",
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "./index.html", to: "./" },
+        { from: "./index.css", to: "./" },
+        { from: "./assets", to: "./assets" },
+      ],
+    }),
+  ],
+};
+
+if (isDev) {
+  config.devtool = "inline-source-map";
+}
+
+module.exports = config;
